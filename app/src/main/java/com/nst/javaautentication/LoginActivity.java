3fee8ed31982975612f8eac85312b7fb8a49ba0a
A1exsandro <alexsandro.acp@gmail.com>
2023-03-13 14:37:15 -0400

    navigate to sign up

--- app/src/main/java/com/nst/javaautentication/LoginActivity.java
+++ app/src/main/java/com/nst/javaautentication/LoginActivity.java
@@ -2,13 +2,24 @@ package com.nst.javaautentication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.nst.javaautentication.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.textSignUp.setOnClickListener(view -> {
+            startActivity(new Intent(this, SignUpActivity.class));
+        });
     }
 }
